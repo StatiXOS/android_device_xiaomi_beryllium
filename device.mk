donboruza/device_beryllium

beryllium: switch to aidl Light HAL
Change-Id: I3775c2348bc9cac8f4290c976c7d09c421278519
Signed-off-by: Chenyang Zhong <zhongcy95@gmail.com>

device.mk
@@ -194,7 +194,7 @@ PRODUCT_PACKAGES += \
 
 # Lights
 PRODUCT_PACKAGES += \
-    android.hardware.light@2.0-service.xiaomi_beryllium
+    android.hardware.lights-service.xiaomi_beryllium
 
 # Media
 PRODUCT_PACKAGES += \

StatiXOS/android_device_xiaomi_beryllium

device.mk
@@ -191,7 +191,7 @@ PRODUCT_PACKAGES += \
 
 # Lights
 PRODUCT_PACKAGES += \
-    android.hardware.light@2.0-service.beryllium
+    android.hardware.lights-service.beryllium
 
 # Media
 PRODUCT_PACKAGES += \